cd97e7c8d5e0457354323562f7809a4321ed0940
DHRUV ITWALA <dhruvitwala@gmail.com>
2023-02-15 20:13:47 +0530

    Player Movement + Camera Movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float CameraSpeed;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(CameraSpeed * Time.deltaTime, 0, 0);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float PlayerSpeed;
+    private Rigidbody2D rb;
+    private Vector2 PlayerDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float DirectionY = Input.GetAxisRaw("Vertical");
+        PlayerDirection = new Vector2(0, DirectionY).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(0, PlayerDirection.y * PlayerSpeed);
+    }
+}
